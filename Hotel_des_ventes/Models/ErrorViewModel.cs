fa17496ed7847c3eea0f6bf284d2c57f60b2b95c
Hugo Poujol <hugo.poujol@epita.fr>
2022-04-29 14:25:12 +0200

    fix: init

---
+++ Hotel_des_ventes/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Hotel_des_ventes.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
