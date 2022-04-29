588a049dcbaa88e3183228b304203ca99e09c815
Eliott <eliott.bru@gmail.com>
2022-04-29 16:25:29 +0100

    Feat : Login page

---
+++ Hotel_des_ventes/Controllers/ConnectionController.cs
@@ -0,0 +1,43 @@
+﻿using Hotel_des_ventes.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Hotel_des_ventes.Controllers
+{
+    public class ConnectionController : Controller
+    {
+        private readonly ILogger<ConnectionController> _logger;
+
+        public ConnectionController(ILogger<ConnectionController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(IFormCollection fc)
+        {
+            //add verif mdp et userId
+            if (ModelState.IsValid)
+            {
+                DateTimeOffset dateTime = DateTimeOffset.Now;
+                dateTime = dateTime.AddDays(1);
+                CookieOptions option = new CookieOptions();
+                option.Expires = dateTime;
+                Response.Cookies.Append("UserID", fc["Id"], option);
+            }
+            return RedirectToAction("Index", "Home");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
