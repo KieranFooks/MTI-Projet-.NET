3f6f491ea62905c48a78328e202419e410b12ea2
Firewop1 <43815727+Firewop@users.noreply.github.com>
2022-04-29 16:42:28 +0200

    Fixed Database project

---
+++ Database/dbo/Tables/Item.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Item] (
+    [Id]          INT  NOT NULL,
+    [Name]        TEXT NOT NULL,
+    [Description] TEXT NULL,
+    CONSTRAINT [PK_Item] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Database/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User] (
+    [Id]       INT  NOT NULL,
+    [Name]     TEXT NOT NULL,
+    [Money]    INT  NOT NULL,
+    [Password] TEXT NOT NULL,
+    CONSTRAINT [PK_User] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
