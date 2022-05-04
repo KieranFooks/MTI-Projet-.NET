41e88efaafa64c891bf1857ceab951693ebcb6a6
Hugo Poujol <hugo.poujol@epita.fr>
2022-05-04 19:23:17 +0200

    feat: Home Page

---
+++ Hotel_des_ventes/Models/HomeViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Hotel_des_ventes.Models
+{
+    public class HomeViewModel
+    {
+        public int selectedItem { get; set; }
+        public List<AnnouncesModel> Announces { get; set; }
+        public List<ItemViewModel> Items { get; set; }
+    }
+}
