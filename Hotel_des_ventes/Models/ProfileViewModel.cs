4d9a15db9581c5d2f6f5d9758aad8a1aba9a38c6
Hugo Poujol <hugo.poujol@epita.fr>
2022-05-04 19:04:48 +0200

    feat: Profile Page

---
+++ Hotel_des_ventes/Models/ProfileViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Hotel_des_ventes.Models
+{
+    public class ProfileViewModel
+    {
+        public string Username { get; set; }
+        public int Money { get; set; }
+        public IEnumerable<ItemViewModel> Items { get; set; }
+        public IEnumerable<AnnouncesModel> Announces { get; set; }
+
+    }
+}
