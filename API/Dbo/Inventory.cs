16e56465e720a9d9a3f1e2151cc5aaf9afe24c24
KieranFooks <kieranfooks@gmail.com>
2022-05-05 20:14:49 +0200

    add item and user to inventory

--- API/Dbo/Inventory.cs
+++ API/Dbo/Inventory.cs
@@ -5,5 +5,8 @@
 		public int IdUser { get; set; }
 		public int IdItem { get; set; }
 		public int Quantity { get; set; }
+
+		public virtual Item IdItemNavigation { get; set; } = null!;
+		public virtual User IdUserNavigation { get; set; } = null!;
 	}
 }
