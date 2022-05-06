2fd01514d5720961881e6bb5cbb12722fe77e646
Firewop1 <43815727+Firewop@users.noreply.github.com>
2022-05-06 12:01:32 +0200

    Added Primary key on Id_user, Id_item

--- Database/dbo/Tables/TInventory.sql
+++ Database/dbo/Tables/TInventory.sql
@@ -2,7 +2,10 @@
     [Id_user]  INT NOT NULL,
     [Id_item]  INT NOT NULL,
     [Quantity] INT NOT NULL,
+    CONSTRAINT [PK_TInventory] PRIMARY KEY CLUSTERED ([Id_user] ASC, [Id_item] ASC),
     CONSTRAINT [FK_Inventory_Item] FOREIGN KEY ([Id_item]) REFERENCES [dbo].[TItem] ([Id]),
     CONSTRAINT [FK_Inventory_User] FOREIGN KEY ([Id_user]) REFERENCES [dbo].[TUser] ([Id])
 );
+
+
 
