50d73b4d05c746394c39e0609b7d1a1c7518344c
KieranFooks <kieranfooks@gmail.com>
2022-05-06 19:24:23 +0200

    Remove Database project

--- Database/dbo/Tables/TItem.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[TItem] (
-    [Id]          INT            IDENTITY (1, 1) NOT NULL,
-    [Name]        NVARCHAR (100) NOT NULL,
-    [Description] TEXT           NULL,
-    CONSTRAINT [PK_Item] PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
-
-
